54e1255a658a34a7699aafcbacceaf160ba9c5e6
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-03-01 09:28:08 +0100

    created singleChannelCard that renders card for each channel

---
+++ components/singleChannelCard.js
@@ -0,0 +1,33 @@
+export const SingleChannelCard = (channel) => {
+  const channelName = channel.title
+  const channelUrl = channel.customUrl
+  const channelImg = channel.thumbnails.default.url
+  const ChannelSubscribers = channel.statistics.subscriberCount
+  const channelVideos = channel.statistics.videoCount
+  const channelViews = channel.statistics.viewCount
+
+  return /* HTML */ ` <a
+    class="card"
+    href="${channelUrl.slice(1, -1)}/?utm_campaign=${'12.12.2022'}"
+    target="blank"
+  >
+    <img src=${channelImg} />
+    <div>
+      <h2>${channelName}</h2>
+      <div class="card__statistics">
+        <div>
+          <p>SUBSCRIBERS</p>
+          <h4>${ChannelSubscribers}</h4>
+        </div>
+        <div>
+          <p>VIDEOS</p>
+          <h4>${channelVideos}</h4>
+        </div>
+        <div>
+          <p>VIEWS</p>
+          <h4>${channelViews}</h4>
+        </div>
+      </div>
+    </div>
+  </a>`
+}
