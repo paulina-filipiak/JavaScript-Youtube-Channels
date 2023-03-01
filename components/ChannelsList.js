8e9b85d15c3836517d8788d64f648d08f9edb9bc
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-03-01 16:03:02 +0100

    styled cards

--- components/ChannelsList.js
+++ components/ChannelsList.js
@@ -2,9 +2,7 @@ import SingleChannelCard from './singleChannelCard.js'
 import { View } from '../utils/View.js'
 
 function ChannelsList(channels) {
-  return (View.innerHTML = `<div>${channels
-    .map((channel) => SingleChannelCard(channel))
-    .join('')}</div>`)
+  return (View.innerHTML = `${channels.map((channel) => SingleChannelCard(channel)).join('')}`)
 }
 
 export default ChannelsList
--- components/singleChannelCard.js
+++ components/singleChannelCard.js
@@ -1,19 +1,19 @@
 function SingleChannelCard(channel) {
   const channelName = channel.title
   const channelUrl = channel.customUrl
-  const channelImg = channel.thumbnails.default.url
+  const channelImg = channel.thumbnails.medium.url
   const ChannelSubscribers = channel.statistics.subscriberCount
   const channelVideos = channel.statistics.videoCount
   const channelViews = channel.statistics.viewCount
 
   return /* HTML */ ` <a
-    class="card"
+    class="wrapper__card"
     href="${channelUrl}/?utm_campaign=${'12.12.2022'}"
     target="blank"
   >
     <img src=${channelImg} />
-    <div>
-      <h2>${channelName}</h2>
+    <div "card__info">
+      <h2 class="card__name">${channelName}</h2>
       <div class="card__statistics">
         <div>
           <p>SUBSCRIBERS</p>
