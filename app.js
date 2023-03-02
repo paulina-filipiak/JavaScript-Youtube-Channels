461a78672e18ea6e4b7958cb9d811546f6ecdf82
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-03-02 11:56:03 +0100

    Added filter on users input

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 import ChannelsList from "./components/ChannelsList.js"
 import  mockData from './static/channels.json' assert {type: 'json'}
 import visitCounter from "./components/visitsCounter.js"
-import {filterData} from "./components/ChannelsListFiltered.js"
+
 
 const App = () => {
-  const searchBar = document.querySelector(".filter__input")
-  console.log('Im working!')
-  console.log('mockData is', mockData)
+  // console.log('Im working!')
+  // console.log('mockData is', mockData)
   visitCounter()
-  filterData(mockData)
-  // if(searchBar.value.length !== 0){
-  //   mockData.filter((channel) => channel.title.includes(query))
-  //       return (View.innerHTML = `${channels.map((channel) => SingleChannelCard(channel)).join('')}`)}
-  
-  return ChannelsList(mockData)
+  const searchBar = document.querySelector(".filter__input")
+  let filteredData = mockData
+  searchBar.addEventListener("keyup", function (e) {
+    const query = e.target.value
+    // console.log(query)
+    filteredData = mockData.filter((channel)=> channel.title.includes(query))
+    // console.log("filtered data is", filteredData)
+    return ChannelsList(filteredData)
+  })
+  if(searchBar.value === ""){return ChannelsList(filteredData)}
 }
 
 export default App()
