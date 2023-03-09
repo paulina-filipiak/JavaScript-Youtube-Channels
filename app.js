a736cf5e56fadb48f7451d8cdd5064d1e4907084
Paulina Filipiak <slodzinska.paulina@gmail.com>
2023-03-09 12:51:48 +0100

    code refactored, sorting data, not DOM elements

--- app.js
+++ app.js
@@ -1,27 +1,58 @@
 import ChannelsList from "./components/ChannelsList.js"
 import mockData from "./static/channels.json" assert { type: "json" }
 import VisitCounter from "./components/VisitsCounter.js"
-import { CardSorter } from "./components/CardSorter.js"
-import { ResetSorter } from "./components/ResetSorter.js"
+import {
+	sortBySubs,
+	sortByTitle,
+	sortByVideos,
+	sortByViews,
+} from "./components/CardSorterThree.js"
 
 const App = () => {
 	VisitCounter()
 	let data = mockData
-	const searchBar = document.querySelector(".filter__input")
-	const checkboxes = document.querySelectorAll(".choice--radio")
-	const clearButton = document.querySelector(".button__clear")
+	let filteredData = JSON.parse(JSON.stringify(data))
 	const form = document.querySelector("form")
-	let filteredData = data
+	const searchBar = document.querySelector(".filter__input")
+	const clearButton = document.querySelector(".button__clear")
+	const reverseButton = document.querySelector(".button__ascending")
+	const checkboxes = document.querySelectorAll(".choice--radio")
+	const checkboxTitle = document.querySelector("#sort-title")
+	const checkboxSubscribers = checkboxes[1]
+	const checkboxVideos = checkboxes[2]
+	const checkboxViews = checkboxes[3]
 
-	clearButton.addEventListener("click", ResetSorter)
+	checkboxTitle.addEventListener("click", () => {
+		sortByTitle(filteredData)
+	})
 
-	checkboxes.forEach((checkbox) =>
-		checkbox.addEventListener("click", CardSorter)
-	)
+	checkboxSubscribers.addEventListener("click", () => {
+		sortBySubs(filteredData)
+	})
+
+	checkboxVideos.addEventListener("click", () => {
+		sortByVideos(filteredData)
+	})
+
+	checkboxViews.addEventListener("click", () => {
+		sortByViews(filteredData)
+	})
+
+	reverseButton.addEventListener("click", () => {
+		filteredData.reverse()
+		return ChannelsList(filteredData)
+	})
+
+	clearButton.addEventListener("click", () => {
+		form.reset()
+		searchBar.value = ""
+		filteredData = JSON.parse(JSON.stringify(data))
+		console.log(" filteredData from clearButton", filteredData)
+		return ChannelsList(data)
+	})
 
 	searchBar.addEventListener("keyup", function (e) {
 		const query = e.target.value
-		console.log(searchBar.value)
 		filteredData = data.filter((channel) =>
 			channel.title.toLowerCase().includes(query.toLowerCase())
 		)
--- components/cardSorter.js
+++ components/cardSorter.js
@@ -1,77 +1,77 @@
-export const CardSorter = (event) => {
-	const checkboxId = event.target.id
-	const cardsFromDOM = document.querySelectorAll(".wrapper__card")
+// export const CardSorter = (event) => {
+// 	const checkboxId = event.target.id
+// 	const cardsFromDOM = document.querySelectorAll(".wrapper__card")
 
-	const sortByTitle = (cardOne, cardTwo) => {
-		return cardOne
-			.getAttribute("data-title")
-			.localeCompare(cardTwo.getAttribute("data-title"))
-	}
+// 	const sortByTitle = (cardOne, cardTwo) => {
+// 		return cardOne
+// 			.getAttribute("data-title")
+// 			.localeCompare(cardTwo.getAttribute("data-title"))
+// 	}
 
-	const sortBySubscribers = (cardOne, cardTwo) => {
-		return (
-			cardTwo.getAttribute("data-subscribers") -
-			cardOne.getAttribute("data-subscribers")
-		)
-	}
+// 	const sortBySubscribers = (cardOne, cardTwo) => {
+// 		return (
+// 			cardTwo.getAttribute("data-subscribers") -
+// 			cardOne.getAttribute("data-subscribers")
+// 		)
+// 	}
 
-	const sortByVideos = (cardOne, cardTwo) => {
-		return (
-			cardTwo.getAttribute("data-videos") - cardOne.getAttribute("data-videos")
-		)
-	}
+// 	const sortByVideos = (cardOne, cardTwo) => {
+// 		return (
+// 			cardTwo.getAttribute("data-videos") - cardOne.getAttribute("data-videos")
+// 		)
+// 	}
 
-	const sortByViews = (cardOne, cardTwo) => {
-		return (
-			cardTwo.getAttribute("data-views") - cardOne.getAttribute("data-views")
-		)
-	}
+// 	const sortByViews = (cardOne, cardTwo) => {
+// 		return (
+// 			cardTwo.getAttribute("data-views") - cardOne.getAttribute("data-views")
+// 		)
+// 	}
 
-	const sortDataByTitle = () => {
-		const cardsArray = Array.from(cardsFromDOM) //method creates a new array from an existing one, or from an array-like object (which is what a NodeList is)
-		let sorted = cardsArray.sort(sortByTitle)
-		sorted.forEach((el) =>
-			document.querySelector(".js-content").appendChild(el)
-		)
-	}
+// 	const sortDataByTitle = () => {
+// 		const cardsArray = Array.from(cardsFromDOM) //method creates a new array from an existing one, or from an array-like object (which is what a NodeList is)
+// 		const sorted = cardsArray.sort(sortByTitle)
+// 		sorted.forEach((el) =>
+// 			document.querySelector(".js-content").appendChild(el)
+// 		)
+// 	}
 
-	const sortDataBySubscribers = () => {
-		const cardsArray = Array.from(cardsFromDOM)
-		let sorted = cardsArray.sort(sortBySubscribers)
-		sorted.forEach((el) =>
-			document.querySelector(".js-content").appendChild(el)
-		)
-	}
+// 	const sortDataBySubscribers = () => {
+// 		const cardsArray = Array.from(cardsFromDOM)
+// 		const sorted = cardsArray.sort(sortBySubscribers)
+// 		sorted.forEach((el) =>
+// 			document.querySelector(".js-content").appendChild(el)
+// 		)
+// 	}
 
-	const sortDataByVideos = () => {
-		const cardsArray = Array.from(cardsFromDOM)
-		let sorted = cardsArray.sort(sortByVideos)
-		sorted.forEach((el) =>
-			document.querySelector(".js-content").appendChild(el)
-		)
-	}
+// 	const sortDataByVideos = () => {
+// 		const cardsArray = Array.from(cardsFromDOM)
+// 		const sorted = cardsArray.sort(sortByVideos)
+// 		sorted.forEach((el) =>
+// 			document.querySelector(".js-content").appendChild(el)
+// 		)
+// 	}
 
-	const sortDataByViews = () => {
-		const cardsArray = Array.from(cardsFromDOM)
-		let sorted = cardsArray.sort(sortByViews)
-		sorted.forEach((el) =>
-			document.querySelector(".js-content").appendChild(el)
-		)
-	}
+// 	const sortDataByViews = () => {
+// 		const cardsArray = Array.from(cardsFromDOM)
+// 		const sorted = cardsArray.sort(sortByViews)
+// 		sorted.forEach((el) =>
+// 			document.querySelector(".js-content").appendChild(el)
+// 		)
+// 	}
 
-	switch (checkboxId) {
-		case "sort-title":
-			sortDataByTitle()
-			break
-		case "sort-subscribers":
-			sortDataBySubscribers()
-			break
-		case "sort-videos":
-			sortDataByVideos()
-			break
-		case "sort-views":
-			sortDataByViews()
-			break
-		default:
-	}
-}
+// 	switch (checkboxId) {
+// 		case "sort-title":
+// 			sortDataByTitle()
+// 			break
+// 		case "sort-subscribers":
+// 			sortDataBySubscribers()
+// 			break
+// 		case "sort-videos":
+// 			sortDataByVideos()
+// 			break
+// 		case "sort-views":
+// 			sortDataByViews()
+// 			break
+// 		default:
+// 	}
+// }
